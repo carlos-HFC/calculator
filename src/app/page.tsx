4352c7bad30e30e336a1f7a3d6c3e359be804337
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-15 10:00:15 -0300

    add components, calculate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,231 @@
-import Image from 'next/image'
+"use client";
+
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+import { Button } from "@/components/Button";
+import { Calculator } from "@/components/Calculator";
+import { Display } from "@/components/Display";
+import { MathSymbol } from "@/components/MathSymbol";
+
+const INITIAL_STATE = {
+  clear: false,
+  operation: null as null | string,
+  values: [0, 0],
+  current: 0
+};
 
 export default function Home() {
+  const [display, setDisplay] = useState("0");
+  const [calc, setCalc] = useState(INITIAL_STATE);
+
+  useEffect(() => {
+    const onKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case 'Backspace':
+          return erase();
+        case 'Enter':
+          return setOperation('=');
+        case 'Escape':
+          return clearDisplay();
+        case '/':
+          return setOperation('/');
+        case '+':
+          return setOperation('+');
+        case '-':
+          return setOperation('-');
+        case '*':
+          return setOperation('*');
+        case ',':
+        case '.':
+          return addCharacter('.');
+        case '0':
+          return addCharacter('0');
+        case '1':
+          return addCharacter('1');
+        case '2':
+          return addCharacter('2');
+        case '3':
+          return addCharacter('3');
+        case '4':
+          return addCharacter('4');
+        case '5':
+          return addCharacter('5');
+        case '6':
+          return addCharacter('6');
+        case '7':
+          return addCharacter('7');
+        case '8':
+          return addCharacter('8');
+        case '9':
+          return addCharacter('9');
+        case 'Dead':
+          return setOperation('pow');
+      }
+    };
+
+    document.addEventListener('keydown', onKeyDown);
+
+    return () => document.removeEventListener('keydown', onKeyDown);
+  });
+
+
+  const error = useMemo(() => {
+    return display === 'Erro';
+  }, [display]);
+
+  const clearDisplay = useCallback(
+    () => {
+      setDisplay('0');
+      setCalc(INITIAL_STATE);
+    },
+    []
+  );
+
+  const erase = useCallback(
+    () => {
+      setDisplay(prev => {
+        const newValue = prev.slice(0, -1) || '0';
+
+        const values = [...calc.values];
+        values[calc.current] = Number(newValue);
+        setCalc(prev => ({ ...prev, values }));
+
+        return newValue;
+      });
+    },
+    [calc, display]
+  );
+
+  const addCharacter = useCallback(
+    (character: string | number) => {
+      if (
+        (character === '.' && display.includes('.')) ||
+        (character === '-' && display.includes('-'))
+      ) return;
+
+      const clear = display === '0' || calc.clear;
+      const current = clear ? '' : display;
+      const displayValue = `${current}${character}`;
+      setDisplay(displayValue);
+      setCalc(prev => ({ ...prev, clear: false }));
+
+      if (character !== '.') {
+        const values = [...calc.values];
+        values[calc.current] = Number(displayValue);
+        setCalc(prev => ({ ...prev, values }));
+      }
+    },
+    [display, calc]
+  );
+
+  const setOperation = useCallback(
+    (operacao: string) => {
+      const BASIC_OPERATION = operacao === '/' || operacao === '-' || operacao === '+' || operacao === '*' || operacao === '=';
+
+      const { current, operation, values } = calc;
+
+      if (BASIC_OPERATION) {
+        if (current === 0) {
+          return setCalc(prev => ({
+            ...prev,
+            operation: operacao,
+            current: 1,
+            clear: true
+          }));
+        }
+
+        switch (operation) {
+          case '+':
+            values[0] = values[0] + values[1];
+            break;
+          case '-':
+            values[0] = values[0] - values[1];
+            break;
+          case '/':
+            values[0] = values[0] / values[1];
+            break;
+          case '*':
+            values[0] = values[0] * values[1];
+            break;
+        }
+      }
+
+      switch (operacao) {
+        case 'sqrt':
+          const sqrtValue = String(Math.sqrt(values[0]));
+          setDisplay(sqrtValue);
+
+          values[0] = Number(sqrtValue);
+          return setCalc(prev => ({
+            ...prev,
+            clear: true,
+            values
+          }));
+        case 'fractal':
+          const fractalValue = String(1 / values[0]);
+          setDisplay(fractalValue);
+
+          values[0] = Number(fractalValue);
+          return setCalc(prev => ({
+            ...prev,
+            clear: true,
+            values
+          }));
+        case 'pow':
+          const powValue = String(Math.pow(values[0], 2));
+          setDisplay(powValue);
+
+          values[0] = Number(powValue);
+          return setCalc(prev => ({
+            ...prev,
+            clear: true,
+            values
+          }));
+      }
+
+      const equals = operacao === '=';
+
+      const newValue = values[0] === Infinity ? 'Erro' : values[0];
+
+      values[1] = 0;
+      setDisplay(String(newValue));
+      setCalc({
+        operation: equals ? null : operacao,
+        current: equals ? 0 : 1,
+        clear: !equals,
+        values
+      });
+    },
+    [calc, display]
+  );
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-center text-center text-white bg-default">
+      <Calculator>
+        <Display value={display} />
+        <Button label="C" bt-type="operation" double onClick={clearDisplay} />
+        <Button disabled={error} label="⌫" bt-type="operation" double onClick={erase} />
+        <Button disabled={error} label={<MathSymbol type="fract" />} bt-type="operation" onClick={() => setOperation('fractal')} />
+        <Button disabled={error} label={<MathSymbol type="pow" />} bt-type="operation" onClick={() => setOperation('pow')} />
+        <Button disabled={error} label={<MathSymbol type="sqrt" />} bt-type="operation" onClick={() => setOperation('sqrt')} />
+        <Button disabled={error} label="&#xf7;" bt-type="operation" onClick={() => setOperation('/')} />
+        <Button disabled={error} label="7" bt-type="number" onClick={() => addCharacter(7)} />
+        <Button disabled={error} label="8" bt-type="number" onClick={() => addCharacter(8)} />
+        <Button disabled={error} label="9" bt-type="number" onClick={() => addCharacter(9)} />
+        <Button disabled={error} label="&#xd7;" bt-type="operation" onClick={() => setOperation('*')} />
+        <Button disabled={error} label="4" bt-type="number" onClick={() => addCharacter(4)} />
+        <Button disabled={error} label="5" bt-type="number" onClick={() => addCharacter(5)} />
+        <Button disabled={error} label="6" bt-type="number" onClick={() => addCharacter(6)} />
+        <Button disabled={error} label="-" bt-type="operation" onClick={() => setOperation('-')} />
+        <Button disabled={error} label="1" bt-type="number" onClick={() => addCharacter(1)} />
+        <Button disabled={error} label="2" bt-type="number" onClick={() => addCharacter(2)} />
+        <Button disabled={error} label="3" bt-type="number" onClick={() => addCharacter(3)} />
+        <Button disabled={error} label="+" bt-type="operation" onClick={() => setOperation('+')} />
+        <Button disabled={error} label="&#xB1;" bt-type="number" onClick={() => setOperation('-')} />
+        <Button disabled={error} label="0" bt-type="number" onClick={() => addCharacter(0)} />
+        <Button disabled={error} label="," bt-type="number" onClick={() => addCharacter('.')} />
+        <Button disabled={error} label="&#x3d;" bt-type="equal" onClick={() => setOperation('=')} />
+      </Calculator>
     </main>
-  )
+  );
 }
