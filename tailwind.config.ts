e2b540e297c976be41c81db4053224a685e51afb
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-15 09:51:43 -0300

    config vars, remove scrollbar and images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -9,12 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        default: 'linear-gradient(to right, rgb(83,105,118), rgb(41,46,73))'
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
