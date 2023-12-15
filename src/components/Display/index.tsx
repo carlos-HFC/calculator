0b84c7978f3d45c405e06b9a3ac2c0d3edbddc07
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-15 09:58:32 -0300

    display component

---
+++ src/components/Display/index.tsx
@@ -0,0 +1,13 @@
+interface DisplayProps {
+  value: string;
+}
+
+export function Display(props: DisplayProps) {
+  return (
+    <div className="overflow-hidden text-6xl py-10 px-4 flex justify-end bg-[#202020] font-semibold col-span-4">
+      <p className="overflow-x-auto overflow-y-hidden font-sans">
+        {props.value}
+      </p>
+    </div>
+  );
+}
