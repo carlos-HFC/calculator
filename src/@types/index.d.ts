fbcd6d89210cd4241605ce2110143e5f62ddb3c4
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-15 16:28:22 -0300

    typing calculator

---
+++ src/@types/index.d.ts
@@ -0,0 +1,1 @@
+export type CalculatorType = "default" | "science";
--- src/components/Calculator/index.tsx
+++ src/components/Calculator/index.tsx
@@ -1,7 +1,19 @@
-interface CalculatorProps extends React.HTMLAttributes<HTMLDivElement> {}
+import { CalculatorType } from "@/@types";
+
+interface CalculatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  type?: CalculatorType;
+}
 
 export function Calculator(props: CalculatorProps) {
+  const variant = {
+    default: "grid-rows-[1fr_repeat(6,_78px)]",
+    science: "grid-rows-[1fr_repeat(7,_78px)]"
+  };
+
   return (
-    <div className="bg-[#202020] p-1 grid gap-1 grid-cols-4 grid-rows-[1fr_repeat(6,_78px)] border border-[#3f4046] rounded-lg w-[480px] h-auto" {...props} />
+    <div
+      className={`bg-[#202020] p-1 grid gap-1 grid-cols-4 ${variant[props?.type ?? 'default']} border border-[#3f4046] rounded-lg w-[480px] h-auto`}
+      {...props}
+    />
   );
 }
