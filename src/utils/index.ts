d8760c6c0f97bf623cc79d011a385f90920dd00f
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-19 13:15:54 -0300

    convert temperature

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,5 @@
+import { TemperatureType } from "@/@types";
+
 export function factorialize(value: number): number {
   if (value < 0) return -1;
   if (value === 0) return 1;
@@ -11,4 +13,27 @@ export function termialize(value: number): number {
 
 export function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
+}
+
+export function convertTemperature(value: number, from: TemperatureType) {
+  switch (from) {
+    case "celsius":
+      return {
+        celsius: `${(value).toFixed(2)} ºC`,
+        fahrenheit: `${((value * (9 / 5)) + 32).toFixed(2)} ºF`,
+        kelvin: `${(value + 273.15).toFixed(2)} K`,
+      };
+    case "fahrenheit":
+      return {
+        celsius: `${((value - 32) * (5 / 9)).toFixed(2)} ºC`,
+        fahrenheit: `${(value).toFixed(2)} ºF`,
+        kelvin: `${((value - 32) * (5 / 9) + 273.15).toFixed(2)} K`,
+      };
+    case "kelvin":
+      return {
+        celsius: `${(value - 237.15).toFixed(2)} ºC`,
+        fahrenheit: `${((value - 273.15) * (9 / 5) + 32).toFixed(2)} ºF`,
+        kelvin: `${(value).toFixed(2)} K`,
+      };
+  }
 }
